a90aaa4bcdfdf974c3b876daf4e02bcb8a35dd60
jyty17 <jerryyeh17@gmail.com>
2020-03-09 16:51:11 -0400

    Minor changes

--- react-gdp/src/components/Header/Header.js
+++ react-gdp/src/components/Header/Header.js
@@ -19,15 +19,17 @@ class Header extends React.Component {
           <img src={logo} alt="logo" className="logo" />
           <h1 className="header-title">NatGDP</h1>
         </div>
-        <div className="navbar">
-          <a href="/">about</a>
-          <a href="/">github</a>
-          <a href="/">contact</a>
-          <div className="search">
-            <input type="text" className="searchbar" placeholder="Search.." />
-            <img src={searchIcon} alt="search" className="searchicon" />
-          </div>
-        </div>
+        {
+        // <div className="navbar">
+        //   <a href="/">about</a>
+        //   <a href="/">github</a>
+        //   <a href="/">contact</a>
+        //   <div className="search">
+        //     <input type="text" className="searchbar" placeholder="Search.." />
+        //     <img src={searchIcon} alt="search" className="searchicon" />
+        //   </div>
+        // </div>
+        }
         
 			</div>
 		)
